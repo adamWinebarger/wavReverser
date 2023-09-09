69cbdc9ebd1383099d314f3aaa10c1893b9818bb
adamwinebarger <adam.k.winebarger@gmail.com>
2023-09-09 19:12:44 -0400

    first commit

---
+++ build/CMakeFiles/wav_reverser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wav_reverser.dir/main.c.o"
+  "CMakeFiles/wav_reverser.dir/main.c.o.d"
+  "wav_reverser"
+  "wav_reverser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wav_reverser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/wav_reverser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for wav_reverser.
+# This may be replaced when dependencies are built.
